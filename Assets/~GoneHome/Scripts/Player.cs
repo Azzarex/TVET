59b85ec1225f5f4f0f11d95ab7456b9250f73e58
aramis.malouf <aramis.malouf@600900J211DE404>
2018-04-10 18:01:34 +1000

    started programming

---
+++ Assets/~GoneHome/Scripts/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+[] - Brackets
+{} - Braces
+() - Parenthesis
+
+Hot keys:
+- Clean Code:   CTRL + K + D
+- Fold Code:    CTRL + M + O
+- UnFold Code:  CTRL + M + P
+*/
+
+public class Player : MonoBehaviour
+{
+    public string message = "Hello World!";
+    float speed = 5;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            print(message);
+        }
+
+    }
+}
